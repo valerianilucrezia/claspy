fb60e9f89a0bb3b6fcf8c6f3fc2e46fdcf385d01
brandon-hastings <65084955+brandon-hastings@users.noreply.github.com>
2024-07-22 13:03:14 +0200

    add unit tests framework

---
+++ multi_variate/tests/test_functions.py
@@ -0,0 +1,33 @@
+from unittest import TestCase, main
+import functions
+
+class TestMultivariateClaSPSegementation(TestCase):
+    def setUp(self) -> None:
+        pass
+
+class TestGetFirstCP(TestCase):
+    pass
+
+class TestCpIsValid(TestCase):
+    pass
+
+class TestMySplit(TestCase):
+    pass
+
+class TestLocalSegmentation(TestCase):
+    pass
+
+class TestTakeFirstCP(TestCase):
+    pass
+
+class TestValidateFirstCP(TestCase):
+    pass
+
+class TestFindCPIterative(TestCase):
+    pass
+
+class TestMAFFindCPIterative(TestCase):
+    pass
+
+# if __name__ == "__main__":
+#     main()
