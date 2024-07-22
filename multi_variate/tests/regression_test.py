35fb652abece40cdc54d6ec2af93f16c90bec368
brandon-hastings <65084955+brandon-hastings@users.noreply.github.com>
2024-07-22 23:00:42 +0200

    add regression test framework

---
+++ multi_variate/tests/regression_test.py
@@ -0,0 +1,12 @@
+# load in text/pkl file of true and pred bps from first iteration of code
+# def load truth...
+
+# load in original data
+# def load data...
+
+# run original data in current main file
+# results = main(original_data)
+
+# check if results have changed. If yes, test fails
+# assert truth.true_bps == results.true_bps
+# assert truth.pred_bps == results.pred_bps
