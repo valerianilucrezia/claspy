23ffc3f2ac5b2fd1ea14c3676fa3b4d1df599b39
brandon-hastings <65084955+brandon-hastings@users.noreply.github.com>
2024-07-30 17:41:21 +0200

    add clustering for segments

--- multi_variate/tests/test_functions.py
+++ multi_variate/tests/test_functions.py
@@ -1,5 +1,15 @@
-from unittest import TestCase, main
+from unittest import TestCase
+import numpy as np
 import multivariate_segmentation as multivariate_segmentation
+from clustering import calculate_cluster_score, construct_confusion_matrix
+from sim_data import sim_data, sim_data_dr
+
+
+# simulate ground truths
+vaf_profile, vaf_cp = sim_data(base_freq_range=(0.25, 0.55), mutant_freq_range=(0.9, 0.95))
+baf_profile, baf_cp = sim_data(base_freq_range=(0.25, 0.55), mutant_freq_range=(0.9, 0.95))
+dr_profile, dr_cp = sim_data(base_freq_range=(0.8, 1.1), mutant_freq_range=(1.5, 2.1))
+
 
 class TestMultivariateClaSPSegementation(TestCase):
     def setUp(self) -> None:
@@ -29,5 +39,12 @@ class TestFindCPIterative(TestCase):
 class TestMAFFindCPIterative(TestCase):
     pass
 
-# if __name__ == "__main__":
-#     main()
+class TestCalculateClusterScore(TestCase):
+    baseline = np.random.uniform(0.1, 0.2, 20)
+    mutant = np.random.uniform(0.5, 0.6, 20)
+    profile = np.concatenate((baseline, mutant, baseline, mutant, baseline), axis=None)
+    ts = np.array((range(100),profile))
+    cp = range(21, 100, 20)
+    labels, scores = calculate_cluster_score(ts, cp)
+    print(cp)
+    print(scores)
